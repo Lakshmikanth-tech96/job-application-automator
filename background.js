066c3dea936b520e24bc8dd3bcd4f3003a2c896f
Lakshmikanth-tech96 <82395801+Lakshmikanth-tech96@users.noreply.github.com>
2025-01-26 18:58:41 -0500

    Initial background.js

---
+++ background.js
@@ -0,0 +1,7 @@
+// background.js: Logic for managing job search automation
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("Job Application Automator Extension Installed!");
+});
+
+// Add further background logic to scrape jobs or handle background tasks
+// For example, you can listen to tab events or capture user actions
