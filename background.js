1717e2e252b3ee9013e563541acf70bbca61bebf
Lakshmikanth-tech96 <82395801+Lakshmikanth-tech96@users.noreply.github.com>
2025-02-22 21:19:21 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,5 @@
-// Listener for messages sent from the popup (index.html)
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // Check if the action is 'searchJobs'
   if (request.action === 'searchJobs') {
-    // Log a message when the button is clicked
     console.log("Searching for jobs...");
-
-    // Here, you can add the functionality to search for jobs in the future.
-    // For now, we are just logging a message to the console.
   }
 });
