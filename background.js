e8c4f9f593b5ebdc2ceec2a3ae780961fce3c09a
Lakshmikanth-tech96 <82395801+Lakshmikanth-tech96@users.noreply.github.com>
2025-02-22 21:37:23 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'searchJobs') {
-    console.log("Searching for jobs...");
-  }
+// background.js content for background tasks
+chrome.runtime.onInstalled.addListener(function() {
+  console.log('Job Search Extension installed');
 });
